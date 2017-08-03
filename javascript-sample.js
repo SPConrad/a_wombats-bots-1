669eb294990d29110a30109cff99e4be2a01891d
SPConrad <sean.conrad@willowtreeapps.com>
2017-08-03 14:44:17 -0400

    it's borked but it should work

--- javascript-sample.js
+++ javascript-sample.js
@@ -14,6 +14,44 @@
                 { action: 'shoot', metadata: {} } :
                 { action: 'smoke', metadata: { direction: smokeDirection } };
 
+            /// nearby tiles are listed in 7 lists, left to right. player is always at local 3,3. nearby will be (2,3), (3,2), (3,4), (4,3)
+            /// look at these to see what is immediately adjacent to wombat
+
+    var wallTypes = ["wood-barrier", "fog", "steel-barrier"];
+    var shootyTypes = ["zakano", "wombat"];
+    var scaryTypes = ["shot", "fog", "poison"]
+
+    const above = state.arena[2][3];
+    const below = state.arena[4][3];
+    const left = state.arena[3][2];
+    const right = state.arena[3][4];
+    const orientation = state.arena[3][3].contents.orientation;
+    var forward = "";
+
+    switch (orientation) {
+        case 'e':
+            forward = right;
+            break;
+        case 'w':
+            forward = left;
+            break;
+        case 'n':
+            forward = above;
+            break;  
+        case 's':
+            forward = below;
+    }
+
+    if (wallTypes.indexOf(forward.contents.type) != -1){
+        const command = { action: 'turn', metadata: turnDirection}
+    } else if (scaryTypes.indexOf(forward.contents.type) != -1){
+        const command = { action: 'shoot', metadata: {} } ;
+    } else if (scaryTypes.indexOf(forward.contents.type) != -1){
+        const command = { action: 'move', metadata: {} }
+    }
+
+    
+
     return {
         command,
         state: {
@@ -21,3 +59,27 @@
         }
     };
 });
+
+
+/*   },
+          "meta": []
+        },
+        {
+          "contents": {
+            "type": "wood-barrier",
+            "hp": 20,
+            "uuid": "f078852a-69f6-4120-933b-24d1370e98bf",
+            "deterioration-level": "low"
+          },
+          "meta": []
+        },
+        {
+          "contents": {
+            "type": "fog"
+          }
+        }
+      ],
+      [
+        {
+          "contents": {
+            "type": "open", */
