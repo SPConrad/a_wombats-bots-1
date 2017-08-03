4ec4343a9c22c62f569e9eb22151d3e8f8798690
SPConrad <sean.conrad@willowtreeapps.com>
2017-08-03 15:13:22 -0400

    Food search

--- javascript-sample.js
+++ javascript-sample.js
@@ -6,7 +6,7 @@
     const smokeDirection = smokeDirections[Math.floor(Math.random() * 5)];
 
     const index = Math.floor(Math.random() * 17);
-    const command = index < 10 ?
+    /*const command = index < 10 ?
         { action: 'move', metadata: {} } :
         index < 12 ?
             { action: 'turn', metadata: { direction: turnDirection } } :
@@ -15,11 +15,19 @@
                 { action: 'smoke', metadata: { direction: smokeDirection } };
 
             /// nearby tiles are listed in 7 lists, left to right. player is always at local 3,3. nearby will be (2,3), (3,2), (3,4), (4,3)
-            /// look at these to see what is immediately adjacent to wombat
+            /// look at these to see what is immediately adjacent to wombat*/
 
-    var wallTypes = ["wood-barrier", "fog", "steel-barrier"];
+    var turnyTypes = ["wood-barrier", "fog", "steel-barrier", "poison", "shot"];
     var shootyTypes = ["zakano", "wombat"];
-    var scaryTypes = ["shot", "fog", "poison"]
+    var goodTypes = ["food"]
+
+
+    const adjacents = [
+        {"direction" : "n", "content" : state.arena[2][3]},
+        {"direction" : "s", "content" : state.arena[4][3]},
+        {"direction" : "w", "content" : state.arena[3][2]},
+        {"direction" : "e", "content" : state.arena[3][4]}
+    ]
 
     const above = state.arena[2][3];
     const below = state.arena[4][3];
@@ -27,6 +35,8 @@
     const right = state.arena[3][4];
     const orientation = state.arena[3][3].contents.orientation;
     var forward = "";
+
+    var goDirection = "";
 
     switch (orientation) {
         case 'e':
@@ -41,15 +51,38 @@
         case 's':
             forward = below;
     }
+
     var command = {}
-    if (wallTypes.indexOf(forward.contents.type) != -1){
-         command = { action: 'turn', metadata: turnDirection}
-    } else if (scaryTypes.indexOf(forward.contents.type) != -1){
-         command = { action: 'shoot', metadata: {} } ;
-    } else if (scaryTypes.indexOf(forward.contents.type) != -1){
-         command = { action: 'move', metadata: {} }
-    } else {
-         command = { action: 'move', metadata: {} }
+    var goForFood = false; 
+    for (var i = 0; i < 4; i ++) {
+        ///0 = n, 1 = s, 2 = w, 3 = e
+        if (goodTypes.indexOf(adjacents[i].content.contents.type) != -1){
+            ///it's a food item, get it
+            goForFood = true;
+            var directionToGo = adjacents[i].direction;
+            if (orientation == directionToGo){
+                command = {action: 'move', metadata : {} };
+            } else {
+                if ((orientation == w && directionToGo == s) || (orientation == s && directionToGo == e) || (orientation == e && directionToGo == n) || (orientation = n && directionToGo == w)){
+                    command = {action: 'turn', metadata : {direction: 'left'}}
+                } else if ((orientation == w && directionToGo == n) || (orientation == n && directionToGo == e) || (orientation == e && directionToGo == s) || (orientation = s && directionToGo == w)){
+                    command = {action: 'turn', metadata : {direction: 'right'}}
+                } else if ((orientation == w && directionToGo == e) || (orientation == e && directionToGo == w) || (orientation == n && directionToGo == s) || (orientation = s && directionToGo == n)){
+                    command = {action: 'turn', metadata : {direction: 'about-face'}}
+                }
+            }
+        }
+    };
+    if (goForFood == false){
+        if (turnyTypes.indexOf(forward.contents.type) != -1){
+            command = {action: 'turn', metadata : {direction: turnDirection}};
+        } else if (scaryTypes.indexOf(forward.contents.type) != -1){
+            command = { action: 'shoot', metadata: {} };
+        } else if (getTypes.indexOf(forward.contents.type) != -1){
+            command = { action: 'move', metadata: {} };
+        } else {
+            command = { action: 'move', metadata: {} };
+        }
     }
 
     
